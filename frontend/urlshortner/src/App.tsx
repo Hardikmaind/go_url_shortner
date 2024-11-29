1f5cb4eae8480553c13203cac2880a0aab28178f
Hardikmaind <maindhardik@gmail.com>
2024-11-30 03:38:11 +0530

    frontend boilerplate made

---
+++ frontend/urlshortner/src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+function App() {
+	// const [count, setCount] = useState(0);
+
+	return (
+		<>
+		<Navbar />
+		<section>
+			
+			
+		<div className="">
+			
+			<h1 className="text-6xl">SHORT LAGANI HAIN !</h1>
+			
+			
+				
+			
+		</div>
+		</section>
+		</>
+	);
+}
+
+export default App;
